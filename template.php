3a14f564db98c4811396dac0956b59f8eb4bf472
kanavugal <kanavugalvijay@gmail.com>
2017-02-26 01:57:33 -0800

    push into dev branch kanavugal

--- template.php
+++ template.php
@@ -6,12 +6,6 @@ function bartik_preprocess_html(&$variables) {
   if (!empty($variables['page']['featured'])) {
     $variables['classes_array'][] = 'featured';
   }
-  if (!empty($variables['page']['triptych_first'])
-    || !empty($variables['page']['triptych_middle'])
-    || !empty($variables['page']['triptych_last'])) {
-    $variables['classes_array'][] = 'triptych';
-  }
-
   if (!empty($variables['page']['footer_firstcolumn'])
     || !empty($variables['page']['footer_secondcolumn'])
     || !empty($variables['page']['footer_thirdcolumn'])
